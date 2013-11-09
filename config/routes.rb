5086a2a7ff3e7ed0d6c7d9ce8567b2e3b28dc4e1
George Gottschalk IV <george.gottschalk@gmail.com>
2013-11-09 12:17:16 -0500

    created home about zazz pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "zazz" => "pages#zazz"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
